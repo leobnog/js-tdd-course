4d57bee00eeefd0d2c871f4881e3cbf0ed4fad5e
Leonardo Nogueira <leonardo.nogueira@cbm.df.gov.br>
2018-08-31 04:15:31 -0300

    teste 3

---
+++ src/main.js
@@ -0,0 +1,9 @@
+const a = 3;
+
+let b = 2;
+b = 4
+const c = 'dfklassssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss';
+
+const abc = () => (a + b + c);
+
+abc();
