c48e062811bae366a164837e93726224ade4f6f9
Leonardo Nogueira <leonardo.nogueira@cbm.df.gov.br>
2018-08-31 04:17:07 -0300

    fixing style

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 const a = 3;
 
 let b = 2;
-b = 4
+b = 4;
 const c = 'dfklassssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss';
 
 const abc = () => (a + b + c);
